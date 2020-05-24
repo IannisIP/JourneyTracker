be34f8cfd3211801fd10c5e7507aa6f831415706
Iannis Paris <parisiannis16@stud.ase.ro>
2020-05-24 14:41:07 +0300

    add maps fragment

---
+++ app/src/main/java/journey/project/fragments/LocationSelectFragment.kt
@@ -0,0 +1,142 @@
+package journey.project.fragments
+
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import journey.project.R
+import journey.project.services.ServiciuLocalizare
+import kotlinx.android.synthetic.main.fragment_location_selector.*
+
+
+class LocationSelectFragment : Fragment(), OnMapReadyCallback {
+
+    lateinit var mapView: MapView
+    lateinit var thisContext: Context
+    var serviciuPornit = false
+    var currentUserLoation : LatLng? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_location_selector, container, false)
+        mapView = view.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        thisContext = this.requireActivity()
+        //daca nu exista, solicitam permisiunea ACCESS_FINE_LOCATION
+        if (ContextCompat.checkSelfPermission(
+                thisContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(thisContext as FragmentActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
+        }
+        val intent = Intent(activity, ServiciuLocalizare::class.java) //Serviciu
+
+        btn_confirm_location.text = "Confirm location"
+
+        if (!serviciuPornit) {
+            thisContext.startService(intent)
+            Toast.makeText(thisContext, "Serviciul a pornit", Toast.LENGTH_LONG).show();
+            serviciuPornit = !serviciuPornit
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+        val intent = Intent(activity, ServiciuLocalizare::class.java) //Serviciu
+
+        if (!serviciuPornit) {
+            thisContext.startService(intent)
+            Toast.makeText(thisContext, "Serviciul a pornit", Toast.LENGTH_LONG).show();
+            serviciuPornit = !serviciuPornit
+        }
+        thisContext.registerReceiver(receptorLocatie, IntentFilter("ACTION_COORD"))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+        val intent = Intent(activity, ServiciuLocalizare::class.java) //Serviciu
+
+        if (serviciuPornit) {
+            thisContext.stopService(intent)
+            Toast.makeText(thisContext, "Serviciul a fost oprit", Toast.LENGTH_LONG).show()
+            serviciuPornit = !serviciuPornit
+        }
+        thisContext.unregisterReceiver(receptorLocatie)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    //receptor de mesaje
+    //primesate coordonatele de la serviciu si le afiseaza prin intermediul controlului dedicat
+    val receptorLocatie = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val coord = intent?.getStringExtra("COORD")
+
+            if (coord != null) {
+                currentUserLoation =
+                    LatLng(coord.split(",")[0].toDouble(), coord.split(",")[1].toDouble())
+            };
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        if (map != null) {
+            map.setOnMapClickListener(GoogleMap.OnMapClickListener {
+                map.clear()
+                map.addMarker(MarkerOptions().position(it).title("Current location"))
+                map.moveCamera(CameraUpdateFactory.newLatLng(it))
+                map.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+
+                currentUserLoation = it
+            })
+
+
+            Handler().postDelayed(
+                {
+                    if(currentUserLoation != null) {
+                        map.addMarker(
+                            MarkerOptions().position(currentUserLoation!!).title("Current location")
+                        )
+                        map.moveCamera(CameraUpdateFactory.newLatLng(currentUserLoation))
+                        map.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+                    }
+                },2000
+            )
+        }
+    }
+}
