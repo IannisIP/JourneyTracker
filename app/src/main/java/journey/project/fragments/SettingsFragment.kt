1c76409861ff95571f8dc2fc8d700d20b2b8f840
Iannis Paris <parisiannis16@stud.ase.ro>
2020-05-27 00:00:54 +0300

    add about us and settings page.

--- app/src/main/java/journey/project/fragments/SettingsFragment.kt
+++ app/src/main/java/journey/project/fragments/SettingsFragment.kt
@@ -1,11 +1,17 @@
 package journey.project.fragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import journey.project.R
+import kotlinx.android.synthetic.main.fragment_settings.*
+import java.io.ByteArrayInputStream
+import java.io.ObjectInputStream
+
 
 class SettingsFragment : Fragment() {
 
@@ -16,7 +22,25 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+    fun bytes2Object(raw: ByteArray?): Any? {
+        val bais = ByteArrayInputStream(raw)
+        val ois = ObjectInputStream(bais)
+        return ois.readObject()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val isChecked = sharedPref.getBoolean("isChecked", true)
+
+        cbSuggestion.isChecked = isChecked
+
+        cbSuggestion.setOnClickListener() {
+            with (sharedPref.edit()) {
+                putBoolean("isChecked", cbSuggestion.isChecked)
+                commit()
+            }
+        }
     }
 }
